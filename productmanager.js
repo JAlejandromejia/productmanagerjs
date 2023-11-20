faf1f990f4b10184ae94573a5f7ffe81c0d01fae
Alejandro MD <jalejandromejiadiaz@gmail.com>
2023-11-20 15:28:01 -0600

    add server express

--- productmanager.js
+++ productmanager.js
@@ -1,11 +1,24 @@
 // Importa el módulo 'fs' para realizar operaciones en el sistema de archivos
 const fs = require('fs');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
 class ProductManager {
     constructor(path) {
         // Inicializa el gestor de productos con la ruta del archivo y un array vacío para almacenar productos
         this.path = path;
         this.products = [];
+        this.loadProducts();
+    }
+
+    async loadProducts() {
+        try {
+            const data = await fs.readFile(this.path, 'utf-8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            console.error('Error al cargar productos:', error);
+        }
     }
 
     // Agrega un nuevo producto al gestor de productos
@@ -95,14 +108,54 @@ class ProductManager {
 // Crea una instancia de ProductManager con la ruta de archivo especificada
 const productManager = new ProductManager('products.json');
 
+app.use(express.json());
+
+// Ruta para obtener todos los productos o limitarlos según el query param
+app.get('/products', async (req, res) => {
+    try {
+        const limit = req.query.limit;
+        const products = productManager.getProducts();
+
+        if (limit) {
+            res.json(products.slice(0, limit));
+        } else {
+            res.json(products);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+// Ruta para obtener un producto por su ID
+app.get('/products/:pid', (req, res) => {
+    try {
+        const productId = parseInt(req.params.pid);
+        const product = productManager.getProductById(productId);
+
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).send('Producto no encontrado');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
+
 // Agrega un producto al gestor
 productManager.addProduct({
-    nombreDelProducto: 'DRIIPP NEGRO',
+    nombreDelProducto: 'SOIREES FOR DAYS GRIS TORNASOL ',
     marca: 'GOORIN BROS',
     descripcion: 'Goorin Bros, la gorra del momento. Este modelo es trucker, cuenta con una malla en la parte de atrás, cuenta con un broche ajustable, que se adapta perfectamente a la medida, y en el frente un parche con un increíble animal salvaje.',
     precio: 'MXN$1,449.00',
-    urlImagen: 'https://2cap.com.mx/pub/media/catalog/product/cache/2325c4080d5d7fd8ee1e3c621132bc99/g/o/gorra_goorin_bros_101-0792_driipp_negro_upc_090625664510-1.jpg',
-    id: '1234',
+    urlImagen: 'https://2cap.com.mx/pub/media/catalog/product/cache/2325c4080d5d7fd8ee1e3c621132bc99/g/o/gorra_goorin_bros_101-0865_soirees_for_days_gris_tornasol_upc_090625532130-1.jpg',
+    id: '2',
     stock: '100',
 });
 
